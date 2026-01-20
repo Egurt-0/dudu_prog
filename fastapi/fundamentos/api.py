7a22b5073181fbb283b55d9f22a4f9faadd85b5d
Egurt-0 <eduardogurovsky@gmail.com>
2026-01-20 06:23:43 -0300

    FastaApi_fundamentos

--- fastapi/fundamentos/api.py
+++ fastapi/fundamentos/api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
-
+from pydantic import BaseModel
+from typing import Optional
 
 app  = FastAPI()
 
+
+@app.get("/")
+def inicio():
+    return 
 
 jogadores = {
     1:{
@@ -10,18 +15,24 @@ jogadores = {
         "idade" : 22,
         "time" : "Coritiba"
     },
-    2:{
+    2:{ 
         "nome" : "Messi",
         "idade" : 33,
         "time": "Barcelona"
     }
 }
 
+class Jogador(BaseModel):
+    nome: str
+    idade: int
+    time:  str
 
-@app.get("/")
-def inicio():
-    return jogadores
 
+
+class Atualiza_jogador(BaseModel):
+    nome: Optional[str] = None
+    idade: Optional[int] = None
+    time: Optional[str] = None
 
 
 @app.get("/get-jogador/{id_jogador}")
@@ -45,6 +56,52 @@ def get_jogador_time(time: str):
 #get-jogador-time?time="Barcelona"
 
 
+
+# API Metodos
+
+
+
+@app.post("/cadastra-jogador/{id_jogador}")
+def cadastra_jogador(id_jogador: int, jogador: Jogador ):
+    if id_jogador in jogadores:
+        return {"Erro": "Jogador ja existente"}
+    jogadores[id_jogador] = jogador
+    return jogadores[id_jogador]
+
+
+@app.delete("/exclui-jogador/{id_jogador}")
+def exlcui_jogador(id_jogador: int):
+    if id_jogador not in jogadores:
+        return {"Erro": "Id nao encontrado"}
+    del jogadores[id_jogador]
+    return {"Mensagem": "Jogador excluido"}
+
+
+
+@app.put("/atualiza-jogador/{id_jogador}")
+def atualiza_jogador(id_jogador: int, jogador: Atualiza_jogador):
+    if id_jogador not in jogadores: 
+        return {"Erro": "Id nao encontrado"}
+    if jogador.nome != None:
+        jogadores[id_jogador]["nome"] = jogador.nome
+    if jogador.idade != None:
+        jogadores[id_jogador]["idade"] = jogador.idade
+    if jogador.time != None:
+        jogadores[id_jogador]["time"] = jogador.time
+
+    return jogadores[id_jogador]
+
+
+
+
+
+
+
+
+
+
+
+
 # Para roda, se ele disser que nao encontrou o arquivo(módulo) 'api' enta use no terminal cd fundamentos
-# dai ele encotrara o arquivo, pois estará procurando denntro da pasta fundamentos
+# dai ele encotrara o arquivo, pois estará procurando dentro da pasta fundamentos
 # para rodar digite uvicorn api:app --reload
